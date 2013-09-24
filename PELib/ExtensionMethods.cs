6ac49c1fa7b698c77dec8af5b2d0a4026bc97300
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2013-09-24 05:30:21 +0000

    In an awkward place, but mostly through reading import table.

--- PELib/ExtensionMethods.cs
+++ PELib/ExtensionMethods.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace PELib
 {
-    class ExtensionMethods
+    internal static class ExtensionMethods
     {
+        /// <summary>
+        /// Reads a null-terminated string from a Stream using a BinaryReader.
+        /// The encoding of the string is determined by that of the BinaryReader
+        /// (passed to its constructor).
+        /// </summary>
+        /// <returns>A string read from the Stream.</returns>
+        public static string ReadNullTerminatedString(this BinaryReader br)
+        {
+            var sb = new StringBuilder();
+            while (true) {
+                var c = br.ReadChar();
+                if (c == 0) break;
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public static string ReadNullTerminatedString(this Stream s, long position, Encoding encoding) {
+            var oldpos = s.Position;
+
+            s.Position = position;
+            var br = new BinaryReader(s, encoding);
+
+            var result = br.ReadNullTerminatedString();
+
+            s.Position = oldpos;
+
+            return result;
+        }
     }
 }
--- PELib/PeFile.cs
+++ PELib/PeFile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -27,7 +28,7 @@ namespace PELib
         public PeFile(Stream stream) {
             var zeroOffset = stream.Position;
 
-            var reader = new BinaryReader(stream);
+            var reader = new BinaryReader(stream, Encoding.ASCII);
             DosHeader = FromBinaryReader<IMAGE_DOS_HEADER>(reader);
 
             // Add 4 bytes to the offset
@@ -40,7 +41,45 @@ namespace PELib
 
             for (int i = 0; i < FileHeader.NumberOfSections; i++ )
                 m_sectionHeaders.Add(new SectionHeader(stream));
+
+
+            if (OptionalHeader.ImportTable != null) {
+                var idir_fo  = RvaToFileOffset(OptionalHeader.ImportTable.VirtualAddress);
+                stream.Seek(zeroOffset + idir_fo, SeekOrigin.Begin);
+
+                while (true) {
+                    var dir = new ImportDirectoryTable(stream);
+                    if (dir.IsNull) break;
+
+                    var pos = stream.Position;
+
+                    var name = stream.ReadNullTerminatedString(zeroOffset + dir.NameRva, Encoding.ASCII);
+
+
+                    stream.Position = zeroOffset + dir.ImportLookupTableRva;
+                    var table = new ImportLookupTable(stream, OptionalHeader.IsPE32Plus);
+
+
+
+
+                    stream.Position = pos;
+                }
+
+            }
+
         }
+
+        uint RvaToFileOffset(uint rva) {
+            // Iterate over the section headers to find the section that has this RVA.
+            var sec = ImageSectionHeaders.SingleOrDefault(sh => (rva > sh.VirtualAddress) && (rva < sh.VirtualAddress + sh.VirtualSize));
+            if (sec == null)
+                throw new Exception("Section with containing VA not found.");
+
+            var off = rva - sec.VirtualAddress;
+            return sec.PointerToRawData + off;
+        }
+
+
 
         /// <summary>
         /// Gets the header of the .NET assembly that called this function
@@ -108,6 +147,116 @@ namespace PELib
 
 
 
+    public class ImportDirectoryTable
+    {
+        /// <summary>The RVA of the import lookup table. This table contains a name or ordinal for each import.</summary>
+        public UInt32 ImportLookupTableRva { get; private set; }
+
+        /// <summary> The stamp that is set to zero until the image is bound. After the image is bound,
+        /// this field is set to the time/data stamp of the DLL.</summary>
+        public UInt32 TimeDateStamp { get; private set; }
+
+        /// <summary>The index of the first forwarder reference.</summary>
+        public UInt32 ForwarderChain { get; private set; }
+
+        /// <summary>The address of an ASCII string that contains the name of
+        /// the DLL. This address is relative to the image base.</summary>
+        public UInt32 NameRva { get; private set; }
+
+        /// <summary> The RVA of the import address table. The contents of this table are identical to
+        /// the contents of the import lookup table until the image is bound.</summary>
+        public UInt32 IatRva { get; private set; }
+
+        public ImportDirectoryTable(Stream stream) {
+            var br = new BinaryReader(stream);
+
+            ImportLookupTableRva = br.ReadUInt32();
+            TimeDateStamp = br.ReadUInt32();
+            ForwarderChain = br.ReadUInt32();
+            NameRva = br.ReadUInt32();
+            IatRva = br.ReadUInt32();
+        }
+
+        public bool IsNull {
+            get {
+                return (ImportLookupTableRva == 0)
+                       && (TimeDateStamp == 0)
+                       && (ForwarderChain == 0)
+                       && (NameRva == 0)
+                       && (IatRva == 0);
+            }
+        }
+    }
+
+
+    public class ImportLookupTable
+    {
+        private readonly List<ImportLookupTableEntry> m_entries = new List<ImportLookupTableEntry>();
+        public IEnumerable<ImportLookupTableEntry> Entries { get { return m_entries; } }
+
+
+        public ImportLookupTable(Stream stream, bool pe32Plus) {
+            var br = new BinaryReader(stream, Encoding.ASCII);
+
+            UInt64 ordinalMask = pe32Plus ? (1u << 63) : (1u << 31);
+
+            while (true) {
+                UInt64 val = pe32Plus ? br.ReadUInt64() : br.ReadUInt32();
+                if (val == 0) break;
+
+                bool useOrd = (val & ordinalMask) != 0;
+                
+                if (useOrd) {
+                    UInt16 ord = (UInt16) (val & 0xFFFF);
+                    m_entries.Add(new OrdinalImportLookupTableEntry(ord));
+                }
+                else {
+                    var pos = stream.Position;
+
+                    UInt32 hintNameRva = (UInt32)(val & 0x7FFFFFFF);
+                    stream.Seek(hintNameRva, SeekOrigin.Begin);
+
+                    var hint = br.ReadUInt16();
+                    var name = br.ReadNullTerminatedString();
+                    m_entries.Add(new NameImportLookupTableEntry(hint, name));
+
+                    stream.Position = pos;
+                }
+            }
+        }
+    }
+
+    public abstract class ImportLookupTableEntry {}
+    public class NameImportLookupTableEntry : ImportLookupTableEntry
+    {
+        public UInt16 Hint { get; private set; }
+        public string Name { get; private set; }
+        public NameImportLookupTableEntry(UInt16 hint, string name) {
+            Hint = hint;
+            Name = name;
+        }
+    }
+
+    public class OrdinalImportLookupTableEntry : ImportLookupTableEntry
+    {
+        public UInt16 Ordinal { get; private set; }
+        public OrdinalImportLookupTableEntry(UInt16 ordinal) {
+            Ordinal = ordinal;
+        }
+    }
+
+    /*
+    public class ImportLookupTableEntry
+    {
+        private UInt32 m_value;
+
+        public bool UseOrdinal { get; private set; }
+
+        public ImportLookupTableEntry(bool useOrdinal) {
+            UseOrdinal = useOrdinal;
+        }
+    }
+    */
 
 
  
