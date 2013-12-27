1e60e260e0c82d8c2346e75bd3f15c196c22674b
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2013-12-27 01:12:28 +0000

    Added auto version information, like FwvBrowser.

---
+++ PEBrowser/Properties/VersionInfo.Template.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+/*
+ * VersionInfo.cs is auto-generated at build-time from VersionInfo.Template.cs.
+ * Changes to VersionInfo.cs will be lost. Instead, modify the template if necessary.
+ */
+
+[assembly: AssemblyVersion("1.0.0.$WCREV$")]
+[assembly: AssemblyFileVersion("1.0.0.$WCREV$")]
+[assembly: AssemblyInformationalVersion(
+    "Build date: $WCNOW=%Y-%m-%d %H:%M:%S$; " + 
+    "Revision date: $WCDATE=%Y-%m-%d %H:%M:%S$; " +
+    "Revision(s) in working copy: $WCRANGE$" +
+    "$WCMODS?; WARNING working copy had uncommitted modifications:$.")]
+
